96741f87672b4a68ae8fd9ce16f83bbd03b6bc18
Jason Pell <jason@pellcorp.com>
2012-08-19 01:21:00 +1000

    added some test cases

--- src/test/java/com/pellcorp/xml/parser/HxmlTokeniserTest.java
+++ src/test/java/com/pellcorp/xml/parser/HxmlTokeniserTest.java
@@ -1,7 +1,9 @@
 package com.pellcorp.xml.parser;
 
 import java.io.StringReader;
-
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class HxmlTokeniserTest {
@@ -14,5 +16,28 @@ public class HxmlTokeniserTest {
 		while (parse.nextToken()) {
 			
 		}
+	}
+	
+	@Test
+	public void testReadAttributes() throws Exception {
+		String template = "<dateOfBirth>\n" + 
+				"	      		<date>\n" + 
+				"		      <year>${year}</year>\n" + 
+				"		      <month>${month}</month>\n" + 
+				"		      <day>${day}</day>\n" + 
+				"	          <invalidValue></invalidValue>\n" + 
+				"		      </date>\n" + 
+				"		    </dateOfBirth>";
+		
+		List<String> startTags = new ArrayList<String>();
+		
+		HxmlTokeniser parse = new HxmlTokeniser(new StringReader(template));
+		while (parse.nextToken()) {
+			if (parse.getTokenType() == HxmlTokeniser.START_TAG || parse.getTokenType() == HxmlTokeniser.EMPTY_TAG) {
+				startTags.add(parse.getTokenName());
+			}
+		}
+		
+		assertEquals(6, startTags.size());
 	}
 }
